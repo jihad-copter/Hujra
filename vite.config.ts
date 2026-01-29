5150fc2492951e4b799a86e7bec4540d1b61d8e1
Jihad Anwar Qadir <info.j89@gmail.com>
2026-01-29 14:53:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Hujra/', // ← گۆڕی بە ناوی repository کەت
+      base: '/', // ← گۆڕی بە ناوی repository کەت
       server: {
         port: 3000,
         host: '0.0.0.0',
